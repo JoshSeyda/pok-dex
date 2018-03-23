db5fe05493208716dd58fd232bdd4cad7d984d9a
Josh Seyda <josh.seyda@gmail.com>
2018-03-23 16:51:13 -0400

    fixed array functionality

--- main.js
+++ main.js
@@ -17,44 +17,51 @@ $(document).ready(function(){
 	}
 }
 
-function createPokemon(name){
+function createPokemon(pokemon){
 	$.ajax({
-	url: `https://pokeapi.co/api/v2/pokemon/${name}/` , 
+	url: `https://pokeapi.co/api/v2/pokemon/${pokemon}/` , 
 	type: "GET" , 
 	dataType: "JSON",
 	success: function(data){
+		//link datapoints to variables
 		let name = data.name ,
+			title = data.name ,
 			pic = data.sprites.front_default ,
-			xP = data.base_experience;	
-		let stat = {},
-			abil = [],
+			xP = data.base_experience,	
+			stat = {},
+			abil = [];
+			hp = data.stats[5].stat.name ,
+			hpLvl = data.stats[5].base_stat,
+			attck = data.stats[4].stat.name ,
+			attckLvl = data.stats[4].base_stat,
+			dfns = data.stats[3].stat.name ,
+			dfnsLVL = data.stats[3].base_stat,
+			spd = data.stats[0].stat.name ,
+			spdLvl = data.stats[0].base_stat;
 
-				let hp = data.stats[5].stat.name ,
-					hpLvl = data.stats[5].base_stat,
-					attck = data.stats[4].stat.name ,
-					attckLvl = data.stats[4].base_stat,
-					dfns = data.stats[3].stat.name ,
-					dfnsLVL = data.stats[3].base_stat,
-					spd = data.stats[0].stat.name ,
-					spdLvl = data.stats[0].base_stat ,
-					ability0 = data.abilities[0].ability.name ,
-					ability1 = data.abilities[1].ability.name ,
-					ability2 = data.abilities[2].ability.name ;
-
+			//set the stat object
 			stat.hp = hpLvl;	
 			stat.attck = attckLvl;
 			stat.dfns = dfnsLVL;
 			stat.spd = spdLvl;	
-			abil =[`${ability0}`,`${ability1}`,`${ability2}`];
 
-		let newP = new Pokemon(name, pic, stat, abil);
-
+			//set the ability array
+			for(i=0;i< data.abilities.length; i++){
+				let ability = data.abilities[i].ability.name;
+				abil.push(`${ability}`);
 			}
+			
+		 title = new Pokemon(name, pic, stat, abil);
+			console.log(title);
 	},
 	error: function(error){
 		console.log(error);
 	}
-	});
+	})
 }
-
+createPokemon('nidoking');
+createPokemon('haunter');
+createPokemon('mewtwo');
 });
+
+
